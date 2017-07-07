4b3ae2025442ed5d1e8caf7bbe76530d422c36cf
Bikesh Maharjan <bikeshmaharjan@Bikeshs-MacBook-Pro.local>
2017-07-07 12:32:24 +0545

    config updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  #new config
-  config.serve_static_assets = true
   
   # Mailer Configurations
   
